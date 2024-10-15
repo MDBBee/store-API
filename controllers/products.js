e35a50d0f09bfff03aa633ebb00871d8b7033ae8
MDBBee <bobbyugbebor@gmail.com>
2024-10-15 12:14:11 +0300

    Concluded the query string params

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const prods = await Product.find({ name: { $regex: "ba", $options: "i" } });
+  const prods = await Product.find({ price: { $gt: 100 } });
   res.status(200).json({ noht: prods.length, sucess: true, data: prods });
 };
 const getAllProducts = async (req, res) => {
@@ -28,12 +28,42 @@ const getAllProducts = async (req, res) => {
 
   if (fields) {
     const fielda = fields.split(",").join(" ");
-    console.log(fielda);
     results = results.select(fielda);
   }
 
   if (numericFilters) {
+    console.log(numericFilters);
+    const mapOptions = {
+      "<": "$lt",
+      "<=": "$lte",
+      "=": "$eq",
+      ">": "$gt",
+      ">=": "$gte",
+    };
+
+    // {price: { $gt: 100 }}
+    const regex = /\b(<|<=|=|>|>=)\b/g;
+    numericFilters
+      .replace(regex, (match) => `-${mapOptions[match]}-`)
+      .split(",")
+      .forEach((opt) => {
+        const options = ["price", "rating"];
+        const [field, operator, value] = opt.split("-");
+
+        if (options.includes(field)) {
+          queryOBJ[field] = { [operator]: Number(value) };
+        }
+      });
+
+    results = Product.find(queryOBJ);
   }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  results.skip(skip).limit(limit);
+
   const products = await results;
 
   res.status(200).json({ noHits: products.length, products });
